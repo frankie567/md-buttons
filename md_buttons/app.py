ccd1da4c362a5df64e178451b13c2f983c894362
François Voron <fvoron@gmail.com>
2022-07-18 09:37:32 +0200

    Pass width as an option and guess font size, not the contrary

--- md_buttons/app.py
+++ md_buttons/app.py
@@ -1,36 +1,45 @@
+from typing import Tuple
 from fastapi import FastAPI, Query, Request, Response
 from fastapi.templating import Jinja2Templates
 from pydantic.color import Color
 from PIL import ImageFont
+
+from md_buttons.settings import settings
 
 
 class SVGImageResponse(Response):
     media_type = "image/svg+xml"
 
 
-HTTP_CACHE_TTL = 24 * 60 * 3600
-
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+
+
+def guess_font_size(width: int, padding_x: int, text: str) -> Tuple[int, int]:
+    font_size = 1
+    while True:
+        font = ImageFont.truetype(settings.font, font_size)
+        text_width, text_height = font.getsize(text)
+        if text_width + padding_x > width:
+            break
+        font_size += 1
+    return font_size, text_height
 
 
 @app.get("/button.svg", response_class=SVGImageResponse)
 def get_button(
     request: Request,
     text: str = Query(),
-    font_size: int = Query(32, alias="fs"),
+    width: int = Query(200, ge=0, alias="w"),
     background_color: Color = Query(Color("#2ecc71"), alias="bg"),
     foreground_color: Color = Query(Color("#ffffff"), alias="fg"),
-    padding_x: int = Query(20, alias="px"),
-    padding_y: int = Query(30, alias="py"),
-    border_radius: int = Query(5, alias="br"),
+    padding_x: int = Query(20, ge=0, alias="px"),
+    padding_y: int = Query(30, ge=0, alias="py"),
+    border_radius: int = Query(5, ge=0, alias="br"),
 ):
-    font = ImageFont.truetype("LiberationSans-Regular.ttf", font_size)
-    width, height = font.getsize(text)
+    font_size, height = guess_font_size(width, padding_x, text)
     width += padding_x
     height += padding_y
-
-    padding_left = int(padding_x / 2)
 
     return templates.TemplateResponse(
         "button.svg.jinja2",
@@ -40,11 +49,10 @@ def get_button(
             "font_size": font_size,
             "width": width,
             "height": height,
-            "padding_left": padding_left,
             "border_radius": border_radius,
             "background_color": background_color,
             "foreground_color": foreground_color,
         },
         media_type="image/svg+xml",
-        headers={"Cache-Control": f"public, max-age={HTTP_CACHE_TTL}, immutable"},
+        headers={"Cache-Control": f"public, max-age={settings.http_cache_ttl}, immutable"},
     )
---
+++ md_buttons/settings.py
@@ -0,0 +1,10 @@
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    font: str = "LiberationSans-Regular.ttf"
+    http_cache_ttl: int = 24 * 60 * 3600
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
